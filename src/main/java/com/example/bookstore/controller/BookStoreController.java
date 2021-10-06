46802828d61445542094b429581d1779f98571cb
gencdevops <gencdevops@gmail.com>
2021-10-06 18:17:48 +0300

    BookOrderRequest dto added

--- src/main/java/com/example/bookstore/controller/BookStoreController.java
+++ src/main/java/com/example/bookstore/controller/BookStoreController.java
@@ -1,8 +1,10 @@
 package com.example.bookstore.controller;
 
+import com.example.bookstore.model.Order;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +15,10 @@ public class BookStoreController {
     @GetMapping
     public ResponseEntity<String> hi() {
         return ResponseEntity.ok("hi");
+    }
+
+    @PostMapping
+    public ResponseEntity<Order> putAnOrder() {
+
     }
 }
